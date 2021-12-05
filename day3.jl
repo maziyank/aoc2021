8a5ef3d64d502243c133895220ae16f2d3218f42
maziyank <maziyank@gmail.com>
2021-12-05 17:57:59 +0700

    fix day3 variable

--- day3.jl
+++ day3.jl
@@ -6,24 +6,25 @@ numbers = [split(x,"") for x in readlines(input)]
 len_x = length(first(numbers)); len_y = length(numbers)
 
 # part 1
-gamma = Vector{Bool}(); epsilon = Vector{Bool}()
+γ = Vector{Bool}(); ϵ = Vector{Bool}()
 for i in 1:len_x
     curr = Vector{Bool}()
     for j in 1:len_y
        append!(curr, parse(Bool,numbers[j][i])) 
     end
         
-    append!(gamma, count(curr) > len_y - count(curr));     
+    append!(γ, count(curr) > len_y - count(curr));     
 end
 
-epsilon = .!gamma
-answer1 = parse(Int, join(map(x -> string(convert(Int64, x)), gamma)), base= 2) * parse(Int, join(map(x -> string(convert(Int64, x)), epsilon)), base= 2)
+ϵ = .!γ
+answer1 = parse(Int, join(map(x -> string(convert(Int64, x)), γ)), base= 2) * parse(Int, join(map(x -> string(convert(Int64, x)), ϵ)), base= 2)
 println("Part 1:", answer1)
 
 
 #part 2
 function get_residual(nums, oxygen = true)
     eliminated = copy(nums)
+    eliminated2 = copy
     for i in 1:len_x     
          curr = Vector{Bool}(); len_y = length(eliminated)
          for j in 1: len_y; append!(curr, parse(Bool,eliminated[j][i])); end
@@ -40,5 +41,5 @@ function get_residual(nums, oxygen = true)
      parse(Int,join(first(eliminated)), base=2)
  end
  
- answer2 = get_residual(numbers, true) * get_residual(numbers, false)
+ answer2 = get_residual(numbers) * get_residual(numbers, false)
  println("Part 2:", answer2)
